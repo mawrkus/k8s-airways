b3a9281612cd908b6bedc05743fbe00b9117a0f4
Marc Mignonsin <marc.mignonsin@softonic.com>
2020-04-18 23:56:20 +0200

    Fixed projects rollback bug

--- K8sCommandsForProjects.js
+++ K8sCommandsForProjects.js
@@ -19,12 +19,14 @@ class K8sCommandsProjects extends K8sCommands {
   }
 
   async listProjectRevisions(release) {
+    this.currentRelease = release;
+
     const execP = this.currentContexts.map(async (context) => {
       const command = `helm history ${release} --kube-context ${context} --namespace ${this.currentNamespace} --max 100 -o json`;
 
       try {
         const stdout = await this.exec(command);
-        const revisions = JSON.parse(stdout);
+        const revisions = this.normalizeRevisions(JSON.parse(stdout));
         return { context, revisions };
       } catch(error) {
         return { context, error };
